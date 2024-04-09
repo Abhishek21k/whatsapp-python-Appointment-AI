f428cf300bf5c47b7c18c044c27473f5453df074
Abhishek21k <abhishek21k15@gmail.com>
2024-04-09 23:42:39 +0530

    first

---
+++ app/utils/clinics.py
@@ -0,0 +1,209 @@
+import requests
+
+
+def get_clinics(latitude, longitude):
+    api_url = " http://localhost:3000/api/getClinics"
+    payload = {
+        "latitude": latitude,
+        "longitude": longitude
+    }
+
+    try:
+        # Make the POST request with an empty JSON body
+        response = requests.post(api_url, json=payload)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+
+            # Extract clinic information from the response
+            clinics = data.get("message", [])
+            return clinics
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return []
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+
+def get_Appointment(clinic_id):
+    api_url = " http://localhost:3000/api/getAppointment"
+    payload = {
+        "clinicUserId": clinic_id
+    }
+
+    try:
+        response = requests.post(api_url, json=payload)
+        if response.status_code == 200:
+            return response.json()
+
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return []
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+
+def add_user_appointment(clinic, time, mobile, date):
+    api_url = " http://localhost:3000/api/addAppointment"
+    payload = {
+        "clinic": clinic,
+        "time": time,
+        "mobile": mobile,
+        "date": date
+    }
+
+    try:
+        response = requests.post(api_url, json=payload)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return "Failed to add appointment"
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return "An error occurred while adding the appointment"
+
+
+def update_appointment(id, clinic, date, time):
+    api_url = " http://localhost:3000/api/updateAppointment"
+    payload = {
+        "id": id,
+        "clinic": clinic,
+        "date": date,
+        "time": time
+    }
+
+    try:
+        response = requests.post(api_url, json=payload)
+        if response.status_code == 200:
+            return response.json().get("message", "Appointment updated successfully")
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return "Failed to update appointment"
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return "An error occurred while adding the appointment"
+
+
+def get_user_appointments_fromServer(mobile):
+    api_url = " http://localhost:3000/api/getUserAppointments"
+    payload = {
+        "mobile": mobile,
+    }
+
+    try:
+        response = requests.post(api_url, json=payload)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return "Failed to Get User appointments"
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return "An error occurred while Getting the appointments"
+
+
+def addUser(mobile, name):
+    api_url = f" http://localhost:3000/api/addUser"
+    payload = {
+        "mobile": mobile,
+        "name": name
+    }
+
+    try:
+        response = requests.post(api_url, json=payload)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return {}
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return {}
+
+
+def getUser(mobile):
+    api_url = f" http://localhost:3000/api/getUser"
+    payload = {
+        "mobile": mobile
+    }
+
+    try:
+        response = requests.post(api_url, json=payload)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return {}
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return {}
+
+
+def updateUser(mobile, name, address):
+    api_url = f" http://localhost:3000/api/updateUser"
+    payload = {
+        "mobile": mobile,
+        "name": name,
+        "address": address
+    }
+
+    try:
+        response = requests.post(api_url, json=payload)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return {}
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return {}
+
+
+def get_conversation(mobile):
+    api_url = f" http://localhost:3000/api/getConversation"
+    payload = {
+        "mobile": mobile
+    }
+
+    try:
+        response = requests.post(api_url, json=payload)
+        if response.status_code == 200:
+            messages = response.json()["message"]
+            return messages
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return []
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+
+def add_conversation(mobile, user, bot):
+    api_url = " http://localhost:3000/api/addConversation"
+    payload = {
+        "mobile": mobile,
+        "user": user,
+        "bot": bot
+    }
+
+    try:
+        response = requests.post(api_url, json=payload)
+        if response.status_code == 200:
+            return response.json()["message"]
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return "Failed to send message"
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return "An error occurred while sending the message"
+
+
+# if __name__ == "__main__":
+#     clinics = get_clinics(0, 0)
+#     print(clinics)
---
+++ app/utils/get_response.py
@@ -0,0 +1,143 @@
+from .clinics import *
+from langchain.tools import tool
+from dotenv import load_dotenv
+from .schema import Clinic, ClinicList
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain_openai import ChatOpenAI
+from langchain import hub
+from langchain.agents import create_openai_functions_agent
+from langchain.agents import AgentExecutor
+
+load_dotenv(override=True)
+
+
+@tool
+def get_user_info(mobile: str) -> str:
+    '''RETURNS THE USER INFO FROM DATABASE FOR THE GIVEN MOBILE NUMBER.'''
+    response = str(getUser(mobile))
+    return response
+
+
+@tool
+def add_new_user(mobile: str, name: str) -> str:
+    '''ADDS A NEW USER IN THE DATABASE.'''
+    response = str(addUser(mobile, name))
+    return response
+
+
+@tool
+def update_existing_user(mobile: str, name: str, address: str) -> str:
+    '''UPDATES AN EXISTING USER IN THE DATABASE.'''
+    response = str(updateUser(mobile, name, address))
+    return response
+
+
+@tool
+def get_clinics_list(latitude: int, longitude: int) -> ClinicList:
+    '''RETURN THE NEAREST 1 CLINIC IN THE DATABASE.GET THE USER COORDINATES FROM DATABASE TO GET THE NEAREST CLINIC.'''
+    clinics = get_clinics(latitude, longitude)
+    return ClinicList(message=clinics, status=200)
+
+
+@tool
+def get_clinic_schedule(clinicId: int) -> str:
+    '''RETURNS SOME SCHEDULES OF THE CLINIC FROM DATABASE FOR THE GIVEN CLINIC ID.'''
+    print(clinicId)
+    response = str(get_Appointment(clinicId))
+    print(response)
+    return response
+
+
+@tool
+def get_user_appointments(mobile: str) -> str:
+    '''RETURNS PENDING AND COMPLETED APPOINTMENTS OF THE USER FROM DATABASE FOR THE GIVEN MOBILE NUMBER.'''
+    response = str(get_user_appointments_fromServer(mobile))
+    return response
+
+
+@tool
+def add_appointment(mobile: str, clinic: str, date: str, time: str) -> str:
+    '''ADDS AN APPOINTMENT FOR THE USER IN THE DATABASE. PROVIDE FULL NAME FOR CLINIC. DATE IS IN yyyy-mm-dd FORMAT.'''
+    response = str(add_user_appointment(clinic,  time, mobile, date))
+    return response
+
+
+@tool
+def update_user_appoitment(id: str, clinic: str, date: str, time: str) -> str:
+    '''UPDATES AN APPOINTMENT FOR THE USER IN THE DATABASE. PROVIDE FULL NAME FOR CLINIC. DATE IS IN yyyy-mm-dd FORMAT.'''
+    response = str(update_appointment(id, clinic, date, time))
+    return response
+
+
+tools = [get_clinics_list, get_clinic_schedule,
+         get_user_info, get_user_appointments,
+         add_appointment, add_new_user,
+         update_existing_user, update_user_appoitment]
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+
+prompt = hub.pull("hwchase17/openai-functions-agent")
+
+agent = create_openai_functions_agent(llm, tools, prompt)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+
+def generate_response(wa_id, name, message_body):
+    messages = [
+        SystemMessage(
+            content=f'''You are a dental clinic appointment schedling chatbot.
+            You cant answer anything other than that.
+            Use the tools provided to you for:
+                getting appointment status for user,
+                or showing the user a clinic's schedule,
+                or available clinics,
+                or booking an appointment for the user,
+                or getting the user's info,
+                or updating an appointment details like date,time,clinic name etc,
+                or getting the user's appointments status like list of completed or pending appointments.
+
+            First check if user is present in database.
+            If user is not found in database, add him using the provided name and number, you can also formally ask for the user address , the address need not to be accurate just a landmark location will work.
+            If user wants to update his info, ask him what field he wants to update and then only update that particular field.
+            While adding an appointment provide the date in yyyy-mm-dd format.
+            While updating an appointment provide the appointment id, clinic name, date and time in the provided format.
+            While getting the clinic schedule provide the clinic name.
+            Use the chat history to get the previous conversation and provide the response accordingly.
+
+            USER'S NAME: {name},
+            MOBILE NUMBER: {wa_id[-12:]},
+
+            Start by saying greetings to the user and giving a short description that you are a Dental CLinic Appointment scheduler.'''
+
+
+        ),
+    ]
+
+    old_messages = get_conversation(wa_id[-12:])
+
+    for coversation in old_messages:
+        messages.append(
+            HumanMessage(
+                content=coversation["user"]
+            )
+
+        )
+        messages.append(
+            AIMessage(
+                content=coversation["bot"]
+            )
+        )
+
+    response = agent_executor.invoke(
+        {
+            "input": message_body,
+            "chat_history": messages,
+        }
+    )
+    print("messages:", messages)
+    # Return text in uppercase
+    resp = str(response["output"])
+    add_conversation(wa_id, message_body, resp)
+
+    return resp
